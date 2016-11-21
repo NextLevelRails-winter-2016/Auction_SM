97d5a3403ed5ce8cb4a68213685148b92dddfdb1
sm8du <sm8du@virginia.edu>
2016-11-21 16:59:03 -0500

    1st

---
+++ app/models/auction.rb
@@ -0,0 +1,8 @@
+class Auction < ApplicationRecord
+  has_one :buyer, class_name: "User"
+  has_one :seller, class_name: "User"
+  has_many :bids
+
+  validates_presence_of :title, :description, :start_date, :end_date
+  
+end
---
+++ spec/factories/auctions.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :auction do
+    start_date "2016-11-19 16:16:02"
+    end_date "2016-11-19 16:16:02"
+    title "MyString"
+    description "MyText"
+  end
+end
